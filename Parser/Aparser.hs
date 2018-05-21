f62c0c0caa852e55509377395af4d0e385ed28b1
Punit Rathore <punitr@gmail.com>
2018-05-20 23:55:38 -0700

    having fun with parsers. implemented a wavefront parser

---
+++ Parser/Aparser.hs
@@ -0,0 +1,54 @@
+module AParser (Parser, runParser, satisfy, char, charStr, posInt) where
+
+import           Control.Applicative
+import           Data.Char
+
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser f
+  where
+    f [] = Nothing
+    f (x:xs)
+        | p x       = Just (x, xs)
+        | otherwise = Nothing
+
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+charStr :: Char -> Parser String
+charStr c = (:) <$> (char c) <*> (pure [])
+
+posInt :: Parser Integer
+posInt = Parser f
+  where
+    f xs
+      | null ns   = Nothing
+      | otherwise = Just (read ns, rest)
+      where (ns, rest) = span isDigit xs
+
+inParser f = Parser . f . runParser
+
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (x,y) = (f x, y)
+
+instance Functor Parser where
+  fmap = inParser . fmap . fmap . first
+
+instance Applicative Parser where
+  pure a = Parser (\s -> Just (a, s))
+  (Parser fp) <*> xp = Parser $ \s ->
+    case fp s of
+      Nothing     -> Nothing
+      Just (f,s') -> runParser (f <$> xp) s'
+
+instance Monad Parser where
+  return = pure
+  (>>=) (Parser ma) f = Parser $ \s ->
+                          case (ma s) of
+                            Just (a, s') -> runParser (f a) s'
+                            Nothing -> Nothing
+
+instance Alternative Parser where
+  empty = Parser (const Nothing)
+  Parser p1 <|> Parser p2 = Parser $ liftA2 (<|>) p1 p2
---
+++ Parser/WaveFrontParser.hs
@@ -0,0 +1,114 @@
+{- CIS 194 HW 11
+   due Monday, 8 April
+-}
+
+module WaveFrontParser where
+
+import AParser
+import Control.Applicative
+import Data.Char
+import Data.Foldable
+import Data.Maybe (catMaybes)
+import Debug.Trace
+
+------------------------------------------------------------
+--  1. Parsing repetitions
+------------------------------------------------------------
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = (oneOrMore p) <|> pure []
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore p = (:) <$> p <*> (zeroOrMore p)
+
+rd = read :: String -> Double
+digit = satisfy isDigit
+
+parsePosIntegerStr = oneOrMore digit
+parseAnyIntegerStr = parsePosIntegerStr <|> (:) <$> char '-' <*> parsePosIntegerStr
+
+parsePosDoubleStr = (mappend <$> (mappend <$> (oneOrMore digit) <*> charStr '.')
+                      <*> (oneOrMore digit))
+                    <|> parsePosIntegerStr
+
+parseAnyDouble = rd <$> (parsePosDoubleStr <|>
+                         ((:) <$> char '-' <*> parsePosDoubleStr))
+
+------------------------------------------------------------
+--  2. Utilities
+------------------------------------------------------------
+
+spaces :: Parser String
+spaces = zeroOrMore (satisfy isSpace)
+
+ident :: Parser String
+ident = (:) <$> (satisfy isAlpha) <*> zeroOrMore ((satisfy isAlphaNum))
+
+------------------------------------------------------------
+--  3. Parsing Wavefront OBJ files
+------------------------------------------------------------
+
+data FaceElement = FaceElement (Maybe Double) (Maybe Double) (Maybe Double) deriving (Show, Read)
+
+data Element = Vertex Double Double Double Double
+             | TC Double Double Double
+             | VNormal Double Double Double
+             | Face FaceElement FaceElement FaceElement deriving (Show, Read)
+
+opt = pure Nothing
+
+optionalDouble = (parseAnyDouble <|> pure 0.0 )
+
+maybeDouble = (parseAnyDouble >>= (\a -> return (Just a)))
+              <|> pure (Nothing)
+
+parseVertex = Vertex <$> optionalDouble <* spaces
+              <*> optionalDouble <* spaces
+              <*> optionalDouble <* spaces
+              <*> optionalDouble
+
+parseTC = TC <$> optionalDouble <* spaces
+          <*> optionalDouble <* spaces
+          <*> optionalDouble
+
+parseVNormal = VNormal <$> optionalDouble <* spaces
+               <*> optionalDouble <* spaces
+               <*> optionalDouble <* spaces
+
+parseFaceElement = FaceElement <$> maybeDouble
+                   <*> ((char '/') *> maybeDouble <|> opt)
+                   <*> ((char '/') *> maybeDouble <|> opt)
+
+parseFace = Face <$> parseFaceElement <* spaces <*> parseFaceElement <* spaces <*> parseFaceElement
+
+parseVLine = char 'v' *> spaces *> parseVertex
+parseTCLine = char 'v' *> char 't' *> spaces *> parseTC
+parseVNormalLine = char 'v' *> char 'n' *> spaces *> parseVNormal
+parseFaceLine = char 'f' *> spaces *> parseFace
+
+
+parseLine :: Parser Element
+parseLine = parseTCLine
+            <|> parseVNormalLine
+            <|> parseVLine
+            <|> parseFaceLine
+
+
+printLine :: (Element, b) -> IO ()
+printLine = putStrLn . show . fst
+
+getElement :: (Element, b) -> String
+getElement = show . fst
+
+elementsToString :: [(Element, a)] -> String
+elementsToString = foldr (\e a -> (getElement e) ++ "\n" ++ a) ""
+
+parseFile :: FilePath -> IO ()
+parseFile path = do
+  ch <- readFile path
+  readFile path
+  let fLines = lines ch
+      pls = catMaybes $ (fmap (\l -> runParser parseLine l) fLines)
+  return pls
+    >>= return . elementsToString
+    >>= writeFile "output.txt"
